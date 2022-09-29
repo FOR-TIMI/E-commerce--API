ee3502e9bcfd34c2f4345128930914da78aaeff4
FOR-TIMI <timiplayskeys@gmail.com>
2022-09-29 03:17:50 -0400

    fixed seeds bug

--- seeds/index.js
+++ seeds/index.js
@@ -6,7 +6,8 @@ const seedProductTags = require('./product-tag-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.query("SET FOREIGN_KEY_CHECKS = 0")
+  await sequelize.sync({ force: true })
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
@@ -19,7 +20,8 @@ const seedAll = async () => {
 
   await seedProductTags();
   console.log('\n----- PRODUCT TAGS SEEDED -----\n');
-
+ 
+  await sequelize.query("SET FOREIGN_KEY_CHECKS = 1")
 
 
   process.exit(0);
